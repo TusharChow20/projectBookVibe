08b7b5f92cb0d1bb6a159cf49f7f181c93107599
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2025-10-06 03:11:14 +0600

    sort

--- src/Pages/ListedBooks/ListedBooks.jsx
+++ src/Pages/ListedBooks/ListedBooks.jsx
@@ -4,8 +4,10 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import { getStoredBook } from "../../utilites/localStorageData";
 import Book from "../Book/Book";
+import { MoveDown } from "lucide-react";
 const ListedBooks = () => {
   const [readDoc, setReadDoc] = useState([]);
+  const [sorting, setSorting] = useState("");
   const { data } = useLoaderData();
   useEffect(() => {
     const stroedData = getStoredBook();
@@ -16,8 +18,36 @@ const ListedBooks = () => {
     setReadDoc(myReadList);
   }, []);
 
+  const handleSort = (sortType) => {
+    setSorting(sortType);
+    if (sortType === "Pages") {
+      const sortedPages = [...readDoc].sort(
+        (a, b) => a.totalPages - b.totalPages
+      );
+      setReadDoc(sortedPages);
+    } else {
+      const sortedPages = [...readDoc].sort((a, b) => a.rating - b.rating);
+      setReadDoc(sortedPages);
+    }
+  };
   return (
     <div>
+      <div className="dropdown flex items-center justify-center mt-10">
+        <div tabIndex={0} role="button" className="btn m-1 bg-green-600 ">
+          Sort By: {sorting ? sorting : ""} <MoveDown></MoveDown>
+        </div>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+        >
+          <li>
+            <a onClick={() => handleSort("Pages")}>Pages</a>
+          </li>
+          <li>
+            <a onClick={() => handleSort("Ratings")}>Ratings</a>
+          </li>
+        </ul>
+      </div>
       <Tabs>
         <TabList>
           <Tab>Read Books</Tab>
