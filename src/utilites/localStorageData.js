ac8f319334e85fb87cd1c6af19004ee8d414548f
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2025-10-06 19:11:36 +0600

    final

--- src/utilites/localStorageData.js
+++ src/utilites/localStorageData.js
@@ -1,6 +1,8 @@
 import { Divide } from "lucide-react";
 import { ToastContainer, toast } from "react-toastify";
-
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+const MySwal = withReactContent(Swal);
 const getStoredBook = () => {
   const storedBook = localStorage.getItem("readBooks");
   if (storedBook) {
@@ -16,12 +18,22 @@ const addToLocalStorage = (id) => {
 
   if (storedBook.includes(id)) {
     toast.error("You Already Read The Book");
+    MySwal.fire({
+      title: "You Already Read The Book",
+      text: "Book is in you Book List. Try New Book.",
+      icon: "info",
+    });
   } else {
     storedBook.push(id);
 
     const storeStr = JSON.stringify(storedBook);
     localStorage.setItem("readBooks", storeStr);
     toast.success("Book Added to Read List!");
+    MySwal.fire({
+      title: "Lets Read the Book!",
+      text: "You Have Done IT",
+      icon: "success",
+    });
   }
 };
 
